ae254c7f7bbe848b0ba647fde42732482b25feb8
Neamar <neamar@neamar.fr>
2016-11-13 20:54:02 +0000

    Added constructor tests

--- test/index.js
+++ test/index.js
@@ -6,85 +6,122 @@ var async = require("async");
 
 var RiotRequest = require('../lib/index.js');
 
-describe("Riot queue requester", function() {
-  var riotRequest = new RiotRequest("fake_key", [100, 100]);
+describe("Riot queue", function() {
+  describe("Constructor", function() {
+    it("should throw when missing Riot api key", function() {
+      assert.throws(
+        function() {
+          /* jshint -W031 */
+          new RiotRequest();
+        },
+        /missing riot api/i);
+    });
 
-  it("should return results on valid reply from Riot's server", function(done) {
-    nock('https://euw.api.pvp.net')
-      .get('/fake')
-      .query(true)
-      .reply({}, {ok: true});
+    it("should throw when passing invalid rate limit parameters", function() {
+      assert.throws(
+        function() {
+          /* jshint -W031 */
+          new RiotRequest("fake", "not an array");
+        },
+        /rate-limit must be an array/i);
+    });
 
-    riotRequest.request('EUW', '/fake', false, function(err, res) {
-      if(err) {
-        return done(err);
-      }
+    it("should throw when passing invalid rate limit parameters", function() {
+      assert.throws(
+        function() {
+          /* jshint -W031 */
+          new RiotRequest("fake", [15]);
+        },
+        /rate-limit must be an array of length 2/i);
+    });
 
-      assert.equal(res.ok, true);
-      done();
+    it("should default to development key when rate limit is unspecifed", function() {
+      var riotRequest = new RiotRequest("fake");
+      assert.equal(riotRequest.rateLimits[0], 10);
+      assert.equal(riotRequest.rateLimits[1], 500);
     });
   });
 
-  it("should retry automatically after a 500", function(done) {
-    nock('https://euw.api.pvp.net')
-      .get('/fake')
-      .query(true)
-      .reply(500, {ok: false});
+  describe("Requester without cache", function() {
+    var riotRequest = new RiotRequest("fake_key", [100, 100]);
 
-    nock('https://euw.api.pvp.net')
-      .get('/fake')
-      .query(true)
-      .reply(200, {ok: true});
+    it("should return results on valid reply from Riot's server", function(done) {
+      nock('https://euw.api.pvp.net')
+        .get('/fake')
+        .query(true)
+        .reply({}, {ok: true});
 
-    riotRequest.request('EUW', '/fake', false, function(err, res) {
-      if(err) {
-        return done(err);
-      }
+      riotRequest.request('EUW', '/fake', false, function(err, res) {
+        if(err) {
+          return done(err);
+        }
 
-      assert.equal(res.ok, true);
-      done();
+        assert.equal(res.ok, true);
+        done();
+      });
     });
-  });
 
-  it("should fail after a second 500", function(done) {
-    nock('https://euw.api.pvp.net')
-      .get('/fake')
-      .query(true)
-      .reply(500, {ok: false});
+    it("should retry automatically after a 500", function(done) {
+      nock('https://euw.api.pvp.net')
+        .get('/fake')
+        .query(true)
+        .reply(500, {ok: false});
 
-    nock('https://euw.api.pvp.net')
-      .get('/fake')
-      .query(true)
-      .reply(500, {ok: true});
+      nock('https://euw.api.pvp.net')
+        .get('/fake')
+        .query(true)
+        .reply(200, {ok: true});
 
-    riotRequest.request('EUW', '/fake', false, function(err) {
-      if(!err) {
-        return done(new Error("Expected an error to occur."));
-      }
+      riotRequest.request('EUW', '/fake', false, function(err, res) {
+        if(err) {
+          return done(err);
+        }
 
-      assert.equal(err.statusCode, 500);
-      done();
+        assert.equal(res.ok, true);
+        done();
+      });
     });
-  });
 
-  it("should retry automatically after a 429", function(done) {
-    nock('https://euw.api.pvp.net')
-      .get('/fake')
-      .query(true)
-      .reply(429, {ok: false}, {'retry-after': '0.01'});
+    it("should fail after a second 500", function(done) {
+      nock('https://euw.api.pvp.net')
+        .get('/fake')
+        .query(true)
+        .reply(500, {ok: false});
 
-    nock('https://euw.api.pvp.net')
-      .get('/fake')
-      .query(true)
-      .reply(200, {ok: true});
+      nock('https://euw.api.pvp.net')
+        .get('/fake')
+        .query(true)
+        .reply(500, {ok: true});
 
-    riotRequest.request('EUW', '/fake', false, function(err, res) {
-      if(err) {
-        return done(err);
-      }
+      riotRequest.request('EUW', '/fake', false, function(err) {
+        if(!err) {
+          return done(new Error("Expected an error to occur."));
+        }
 
-      assert.equal(res.ok, true);
-      done();
+        assert.equal(err.statusCode, 500);
+        done();
+      });
+    });
+
+    it("should retry automatically after a 429", function(done) {
+      nock('https://euw.api.pvp.net')
+        .get('/fake')
+        .query(true)
+        .reply(429, {ok: false}, {'retry-after': '0.01'});
+
+      nock('https://euw.api.pvp.net')
+        .get('/fake')
+        .query(true)
+        .reply(200, {ok: true});
+
+      riotRequest.request('EUW', '/fake', false, function(err, res) {
+        if(err) {
+          return done(err);
+        }
+
+        assert.equal(res.ok, true);
+        done();
+      });
     });
   });
 

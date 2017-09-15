c2378c724643b4006185e471855f96f067cbb3cb
Neamar <neamar@neamar.fr>
2017-09-15 18:20:52 +0100

    Resets concurrency when new bucket starts
    When a new bucket starts, keep the old concurrency, do one request and
    use the rate limit for this request as the new default concurrency
    instead of resetting to defaultConcurrency automatically

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 "use strict";
 
 var R = require('./lib');
-var r = new R("RGAPI-c0590c2b-6b37-44c0-bf3d-dec766933649");
+var r = new R("RGAPI-fab5f566-8006-4622-af8a-16fc8e353969");
 
 function doRequest() {
   r.request('br1', 'summoner', '/lol/league/v3/positions/by-summoner/3269809', function() {
